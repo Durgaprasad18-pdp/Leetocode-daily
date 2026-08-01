e5f0ea18ed1888ee55d7610c50f6a05e056634fb
Pala Durga Prasad <pdp.paladurga@gmail.com>
2026-08-01 15:58:46 +0530

    Time: 55 ms (8.78%), Space: 31.4 MB (43.66%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        psum = []
+        psum.append(nums[0])
+        for i in range(1,len(nums)-1+1,1):
+            x = max(psum[i-1]+nums[i],nums[i])
+            psum.append(x)
+        return max(psum)    
+        
