fda360c0ebae5e2a3614e259ffdd0a94ee5f8850
Pala Durga Prasad <pdp.paladurga@gmail.com>
2026-08-05 21:36:07 +0530

    Time: 0 ms (100%), Space: 19.1 MB (86.34%) - LeetHub

---
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        counter = 0
+
+        while n > 0:
+            counter += n & 1
+            n = n >> 1
+        
+        return counter
+        
